d6e634fcccb4f775bd619675cc7ac0aeb6b5aad3
flex5hybrid <113517899+flex5hybrid@users.noreply.github.com>
2026-04-07 00:40:19 +0300

    Update publish_multi_request.py

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,7 +15,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "http://91.210.107.68:8080/"
+ROBUST_CDN_URL = "https://neonify.ru/"
 FORK_ID = "rumc"
 
 def main():
