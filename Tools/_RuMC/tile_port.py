1c278d5e9cd8e02730ce81e44dee138c36fd4004
flex5hybrid <113517899+flex5hybrid@users.noreply.github.com>
2026-04-16 02:55:08 +0300

    tiles

--- Tools/_RuMC/tile_port.py
+++ Tools/_RuMC/tile_port.py
@@ -1,12 +1,12 @@
 from PIL import Image
 
-name = "hunter_tile_1"
+name = "outerhull_dir"
 path = "Resources/Textures/_RuMC14/Tiles/hunter/hunter_floors.rsi"
 img = Image.open(f"{path}/{name}.png")
 tiles = []
 
-for y in range(0, 64, 32):
-    for x in range(0, 64, 32):
+for y in range(0, 96, 32):
+    for x in range(0, 96, 32):
         tiles.append(img.crop((x, y, x+32, y+32)))
 
 out = Image.new("RGBA", (32 * len(tiles), 32))
