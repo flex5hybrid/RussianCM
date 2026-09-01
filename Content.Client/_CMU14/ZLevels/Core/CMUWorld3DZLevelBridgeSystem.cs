7742cc9690c67d880eceb022a9fa9bc956d1a554
Foxxpress <113517899+flex5hybrid@users.noreply.github.com>
2026-09-01 23:07:04 +0300

    Make 3D z-level bridge imports explicit

--- Content.Client/_CMU14/ZLevels/Core/CMUWorld3DZLevelBridgeSystem.cs
+++ Content.Client/_CMU14/ZLevels/Core/CMUWorld3DZLevelBridgeSystem.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using Content.Shared._CMU14.ZLevels.Core.Components;
 using Robust.Client.GameObjects;
 using Robust.Client.Player;
 using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
 using Robust.Shared.Map;
 using Robust.Shared.Map.Components;
 
